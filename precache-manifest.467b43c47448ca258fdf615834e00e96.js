8ae083ad9cddbf0fa69af2085343bec68eac78fe
Swapnil031 <swapnilaudichya@gmail.com>
2020-07-26 11:25:06 +0530

    Updates

---
+++ precache-manifest.467b43c47448ca258fdf615834e00e96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19ceb31af8902505132360c6d6dfc37",
+    "url": "/chat-app-client/index.html"
+  },
+  {
+    "revision": "7d5d88e581f4df151000",
+    "url": "/chat-app-client/static/css/main.5572411a.chunk.css"
+  },
+  {
+    "revision": "2336366ae5b2d815aada",
+    "url": "/chat-app-client/static/js/2.120fadeb.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chat-app-client/static/js/2.120fadeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d5d88e581f4df151000",
+    "url": "/chat-app-client/static/js/main.0892db13.chunk.js"
+  },
+  {
+    "revision": "241a73e1816cac1c076a",
+    "url": "/chat-app-client/static/js/runtime-main.116693f3.js"
+  },
+  {
+    "revision": "47c8665fec76a1a60aaa8c75b3096581",
+    "url": "/chat-app-client/static/media/chat-app.47c8665f.ico"
+  }
+]);
